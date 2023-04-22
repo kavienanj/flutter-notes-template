1bd97df1185f792eb700d20c8b55814a035340a1
kavienanj <kavienanj@gmail.com>
2023-04-22 16:51:44 +0530

    changed actions menu to team actions

--- lib/views/home/notes_screen.dart
+++ lib/views/home/notes_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_notes_template/bloc/user_bloc/user_bloc.dart';
-import 'package:flutter_notes_template/views/auth/signin_screen.dart';
+import 'package:flutter_notes_template/views/widgets/core/actions_menu.dart';
 import 'package:flutter_notes_template/views/widgets/notes/user_notes_view.dart';
 import 'package:flutter_notes_template/views/widgets/teams/teams_view.dart';
 
@@ -10,26 +8,12 @@ class NotesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final userBloc = context.read<UserBloc>();
-    final userEmail = (userBloc.state as UserSuccess).user.email!;
     return Scaffold(
       appBar: AppBar(
         title: const Text("Home"),
-        actions: [
-          CircleAvatar(child: Text(userEmail[0].toUpperCase())),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ElevatedButton(
-              onPressed: () {
-                context.read<UserBloc>().add(UserSignOut());
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => const SignInScreen()),
-                );
-              },
-              child: const Text("Sign Out"),
-            ),
-          ),
+        actions: const [
+          ActionsPopupMenu(),
+          SizedBox(width: 30),
         ],
       ),
       body: ListView(
---
+++ lib/views/widgets/core/actions_menu.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_notes_template/bloc/user_bloc/user_bloc.dart';
+import 'package:flutter_notes_template/views/auth/signin_screen.dart';
+
+class ActionsPopupMenu extends StatelessWidget {
+  const ActionsPopupMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final userBloc = context.read<UserBloc>();
+    final userEmail = (userBloc.state as UserSuccess).user.email!;
+    return PopupMenuButton<int>(
+      offset: const Offset(0, 55),
+      itemBuilder: (context) => [
+        PopupMenuItem(
+          value: 0,
+          child: Row(
+            children: const [
+              Icon(Icons.add, color: Colors.black),
+              SizedBox(width: 10),
+              Text(
+                "Create New Team",
+                style: TextStyle(color: Colors.black),
+              ),
+            ],
+          ),
+        ),
+        PopupMenuItem(
+          value: 1,
+          child: Row(
+            children: const [
+              Icon(Icons.logout, color: Colors.black),
+              SizedBox(width: 10),
+              Text(
+                "Sign Out",
+                style: TextStyle(color: Colors.black),
+              ),
+            ],
+          ),
+        ),
+      ],
+      onSelected: (i) {
+        if (i == 1) {
+          context.read<UserBloc>().add(UserSignOut());
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => const SignInScreen()),
+          );
+        }
+      },
+      child: Row(
+        children: [
+          CircleAvatar(child: Text(userEmail[0].toUpperCase())),
+          const Icon(Icons.arrow_drop_down),
+        ],
+      ),
+    );
+  }
+}
