a4ac68fa4df28983c0d35313d5cb5e2000731bcb
kavienanj <kavienanj@gmail.com>
2023-04-23 13:10:15 +0530

    team member roles change complete

--- lib/views/widgets/teams/team_member_edit.dart
+++ lib/views/widgets/teams/team_member_edit.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_notes_template/bloc/team_bloc/team_bloc.dart';
 import 'package:flutter_notes_template/models/team.dart';
+import 'package:flutter_notes_template/models/team_member.dart';
 import 'package:flutter_notes_template/services/firebase_service.dart';
 import 'package:flutter_notes_template/views/widgets/core/form_fields.dart';
 
@@ -15,6 +16,23 @@ class TeamMemberPermissionsEditField extends StatelessWidget {
   final String userEmail;
   final Team team;
   final bool editAdmins;
+
+  static const String kAdmin = 'Admin';
+  static const String kEditor = 'Editor';
+  static const String kViewer = 'View Only';
+
+  TeamMemberRole getRole(String role) {
+    switch (role) {
+      case kAdmin:
+        return TeamMemberRole.admin;
+      case kEditor:
+        return TeamMemberRole.editor;
+      case kViewer:
+        return TeamMemberRole.viewer;
+      default:
+        return TeamMemberRole.viewer;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,19 +50,26 @@ class TeamMemberPermissionsEditField extends StatelessWidget {
             Text(teamMember.userEmail),
             const SizedBox(width: 16),
             if (!editAdmins && teamMember.isAdmin)
-              const FixedValueDropdown(value: 'Admin')
+              const FixedValueDropdown(value: kAdmin)
             else
               DropdownButton(
                 value: teamMember.hasEditPermission
-                  ? teamMember.isAdmin ? 'Admin' : 'Edit'
-                  : 'View Only',
-                items: ["View Only", "Edit", if (editAdmins) "Admin"].map(
+                  ? teamMember.isAdmin ? kAdmin : kEditor
+                  : kViewer,
+                items: [kViewer, kEditor, if (editAdmins) kAdmin].map(
                   (e) => DropdownMenuItem(
                     value: e,
                     child: Text(e),
                   ),
                 ).toList(),
-                onChanged: (value) {},
+                onChanged: (value) {
+                  teamBloc.add(
+                    TeamMemberRoleChange(
+                      teamMember: teamMember,
+                      role: getRole(value!),
+                    ),
+                  );
+                },
               ),
             if (editAdmins || !teamMember.isAdmin) IconButton(
               splashRadius: 12,
